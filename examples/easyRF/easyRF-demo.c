c25d6ef92c285b7ce3a778395a8935cacccd762a
Matthijs Boelstra <matthijs.boelstra@gmail.com>
2014-10-27 12:40:32 +0100

    wait for a RPL route before doing http requests

--- examples/easyRF/easyRF-demo.c
+++ examples/easyRF/easyRF-demo.c
@@ -3,6 +3,8 @@
 #include "http-socket.h"
 #include "log.h"
 
+static uint8_t has_rpl_route = 0;
+
 /*---------------------------------------------------------------------------*/
 PROCESS(easyRF_demo_process, "EasyRF Demo Process");
 AUTOSTART_PROCESSES(&easyRF_demo_process);
@@ -16,26 +18,43 @@ void http_socket_callback(struct http_socket *s,
   TRACE("ev: %d, datalen: %d", ev, datalen);
 }
 /*---------------------------------------------------------------------------*/
+static void
+rpl_route_callback(int event, uip_ipaddr_t *route, uip_ipaddr_t *ipaddr,
+                   int numroutes)
+{
+  if(event == UIP_DS6_NOTIFICATION_DEFRT_ADD) {
+    if (!has_rpl_route) {
+      has_rpl_route = 1;
+      INFO("Got first RPL route (num routes = %d)", numroutes);
+    } else {
+      INFO("Got RPL route (num routes = %d)", numroutes);
+    }
+  }
+}
+/*---------------------------------------------------------------------------*/
 PROCESS_THREAD(easyRF_demo_process, ev, data)
 {
+  static struct uip_ds6_notification n;
   static struct etimer et;
   static struct http_socket hs;
 
   PROCESS_BEGIN();
 
-  etimer_set(&et, CLOCK_SECOND * 10);
-  PROCESS_WAIT_UNTIL(etimer_expired(&et));
-
   INFO("EasyRF Demo Process started");
 
-  etimer_restart(&et);
+  uip_ds6_notification_add(&n, rpl_route_callback);
+
+  etimer_set(&et, CLOCK_SECOND * 10);
 
   while (1) {
-    PROCESS_YIELD();
-    if(etimer_expired(&et)) {
+    PROCESS_WAIT_UNTIL(etimer_expired(&et));
+
+    if (has_rpl_route) {
+      INFO("****** BEFORE GET ******");
       http_socket_get(&hs, "http://192.168.1.101:9000/api/things", http_socket_callback, 0);
-      etimer_restart(&et);
     }
+
+    etimer_restart(&et);
   }
 
   PROCESS_END();
